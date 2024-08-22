7dfd1fbaf1525cd3049dad100c9cc504270e4a4a
Nahuel Gigena <nahuel.gigena@hotmail.com>
2024-08-22 01:17:45 -0300

    ADD:Load months

--- 2-Vectores/Dos-Vectores/Form1.Designer.cs
+++ 2-Vectores/Dos-Vectores/Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace Dos_Vectores
 {
-    partial class Form1
+    partial class expenseManager
     {
         /// <summary>
         ///  Required designer variable.
@@ -29,17 +29,17 @@
         private void InitializeComponent()
         {
             groupBox1 = new GroupBox();
-            groupBox2 = new GroupBox();
-            label1 = new Label();
-            label2 = new Label();
-            textBox1 = new TextBox();
-            comboBox1 = new ComboBox();
             button1 = new Button();
-            button2 = new Button();
-            button3 = new Button();
+            comboBoxAmount = new ComboBox();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            label1 = new Label();
+            groupBox2 = new GroupBox();
             dataGridView1 = new DataGridView();
             ColumnMonth = new DataGridViewTextBoxColumn();
             ColumnAmount = new DataGridViewTextBoxColumn();
+            button3 = new Button();
+            button2 = new Button();
             groupBox1.SuspendLayout();
             groupBox2.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
@@ -48,7 +48,7 @@
             // groupBox1
             // 
             groupBox1.Controls.Add(button1);
-            groupBox1.Controls.Add(comboBox1);
+            groupBox1.Controls.Add(comboBoxAmount);
             groupBox1.Controls.Add(textBox1);
             groupBox1.Controls.Add(label2);
             groupBox1.Controls.Add(label1);
@@ -58,6 +58,48 @@
             groupBox1.TabIndex = 0;
             groupBox1.TabStop = false;
             groupBox1.Text = "Cargar gasto";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(185, 99);
+            button1.Name = "button1";
+            button1.Size = new Size(84, 29);
+            button1.TabIndex = 4;
+            button1.Text = "Guardar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // comboBoxAmount
+            // 
+            comboBoxAmount.FormattingEnabled = true;
+            comboBoxAmount.Location = new Point(72, 27);
+            comboBoxAmount.Name = "comboBoxAmount";
+            comboBoxAmount.Size = new Size(197, 23);
+            comboBoxAmount.TabIndex = 3;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(72, 61);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(197, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(15, 64);
+            label2.Name = "label2";
+            label2.Size = new Size(49, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Importe";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(15, 27);
+            label1.Name = "label1";
+            label1.Size = new Size(29, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Mes";
             // 
             // groupBox2
             // 
@@ -70,66 +112,6 @@
             groupBox2.TabIndex = 1;
             groupBox2.TabStop = false;
             groupBox2.Text = "Listado de gastos";
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(15, 27);
-            label1.Name = "label1";
-            label1.Size = new Size(29, 15);
-            label1.TabIndex = 0;
-            label1.Text = "Mes";
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(15, 64);
-            label2.Name = "label2";
-            label2.Size = new Size(49, 15);
-            label2.TabIndex = 1;
-            label2.Text = "Importe";
-            // 
-            // textBox1
-            // 
-            textBox1.Location = new Point(72, 61);
-            textBox1.Name = "textBox1";
-            textBox1.Size = new Size(197, 23);
-            textBox1.TabIndex = 2;
-            // 
-            // comboBox1
-            // 
-            comboBox1.FormattingEnabled = true;
-            comboBox1.Location = new Point(72, 27);
-            comboBox1.Name = "comboBox1";
-            comboBox1.Size = new Size(197, 23);
-            comboBox1.TabIndex = 3;
-            // 
-            // button1
-            // 
-            button1.Location = new Point(185, 99);
-            button1.Name = "button1";
-            button1.Size = new Size(84, 29);
-            button1.TabIndex = 4;
-            button1.Text = "Guardar";
-            button1.UseVisualStyleBackColor = true;
-            // 
-            // button2
-            // 
-            button2.Location = new Point(15, 222);
-            button2.Name = "button2";
-            button2.Size = new Size(102, 29);
-            button2.TabIndex = 5;
-            button2.Text = "Listar con for";
-            button2.UseVisualStyleBackColor = true;
-            // 
-            // button3
-            // 
-            button3.Location = new Point(167, 222);
-            button3.Name = "button3";
-            button3.Size = new Size(102, 29);
-            button3.TabIndex = 6;
-            button3.Text = "Listar con while";
-            button3.UseVisualStyleBackColor = true;
             // 
             // dataGridView1
             // 
@@ -150,15 +132,34 @@
             ColumnAmount.HeaderText = "Importe";
             ColumnAmount.Name = "ColumnAmount";
             // 
-            // Form1
+            // button3
+            // 
+            button3.Location = new Point(167, 222);
+            button3.Name = "button3";
+            button3.Size = new Size(102, 29);
+            button3.TabIndex = 6;
+            button3.Text = "Listar con while";
+            button3.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(15, 222);
+            button2.Name = "button2";
+            button2.Size = new Size(102, 29);
+            button2.TabIndex = 5;
+            button2.Text = "Listar con for";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // expenseManager
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(309, 456);
             Controls.Add(groupBox2);
             Controls.Add(groupBox1);
-            Name = "Form1";
+            Name = "expenseManager";
             Text = "Administrador de gastos";
+            Load += expenseManager_Load;
             groupBox1.ResumeLayout(false);
             groupBox1.PerformLayout();
             groupBox2.ResumeLayout(false);
@@ -171,7 +172,7 @@
         private GroupBox groupBox1;
         private GroupBox groupBox2;
         private Button button1;
-        private ComboBox comboBox1;
+        private ComboBox comboBoxAmount;
         private TextBox textBox1;
         private Label label2;
         private Label label1;
--- 2-Vectores/Dos-Vectores/Form1.cs
+++ 2-Vectores/Dos-Vectores/Form1.cs
@@ -1,10 +1,26 @@
 namespace Dos_Vectores
 {
-    public partial class Form1 : Form
+    public partial class expenseManager : Form
     {
-        public Form1()
+        public expenseManager()
         {
             InitializeComponent();
+        }
+
+        String[] months = new String[12] { "Enero", "Febrero", "Marzo", "Abril", "Mayo", "Junio", "Julio", "Agosto", "Septiembre", "Octubre", "Noviembre", "Diciembre" };
+        Decimal[] amounts = new Decimal[12] { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 };
+
+        private void expenseManager_Load(object sender, EventArgs e)
+        {
+            comboBoxAmount.Items.Clear();
+
+            foreach (string month in months)
+            {
+                comboBoxAmount.Items.Add(month);
+            }
+
+            comboBoxAmount.SelectedIndex = 0;
+
         }
     }
 }
