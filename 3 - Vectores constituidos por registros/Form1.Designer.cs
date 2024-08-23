31a743e2c55f6cb17d87cac63cb1c2f352d83a2e
Nahuel Gigena <nahuel.gigena@hotmail.com>
2024-08-23 02:09:18 -0300

    ADD:Create client

--- 3 - Vectores constituidos por registros/Form1.Designer.cs
+++ 3 - Vectores constituidos por registros/Form1.Designer.cs
@@ -28,12 +28,203 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            groupBoxLoadClient = new GroupBox();
+            buttonLoad = new Button();
+            label4 = new Label();
+            textBoxLimitCredit = new TextBox();
+            label3 = new Label();
+            textBoxName = new TextBox();
+            label2 = new Label();
+            textBoxDebt = new TextBox();
+            label1 = new Label();
+            textBoxCode = new TextBox();
+            groupBox1 = new GroupBox();
+            dataGridView1 = new DataGridView();
+            ColumnCode = new DataGridViewTextBoxColumn();
+            ColumnName = new DataGridViewTextBoxColumn();
+            ColumnDebt = new DataGridViewTextBoxColumn();
+            ColumnLimitCredit = new DataGridViewTextBoxColumn();
+            button2 = new Button();
+            groupBoxLoadClient.SuspendLayout();
+            groupBox1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // groupBoxLoadClient
+            // 
+            groupBoxLoadClient.Controls.Add(buttonLoad);
+            groupBoxLoadClient.Controls.Add(label4);
+            groupBoxLoadClient.Controls.Add(textBoxLimitCredit);
+            groupBoxLoadClient.Controls.Add(label3);
+            groupBoxLoadClient.Controls.Add(textBoxName);
+            groupBoxLoadClient.Controls.Add(label2);
+            groupBoxLoadClient.Controls.Add(textBoxDebt);
+            groupBoxLoadClient.Controls.Add(label1);
+            groupBoxLoadClient.Controls.Add(textBoxCode);
+            groupBoxLoadClient.Location = new Point(12, 12);
+            groupBoxLoadClient.Name = "groupBoxLoadClient";
+            groupBoxLoadClient.Size = new Size(533, 198);
+            groupBoxLoadClient.TabIndex = 0;
+            groupBoxLoadClient.TabStop = false;
+            groupBoxLoadClient.Text = "Carga de datos";
+            // 
+            // buttonLoad
+            // 
+            buttonLoad.Location = new Point(392, 153);
+            buttonLoad.Name = "buttonLoad";
+            buttonLoad.Size = new Size(121, 28);
+            buttonLoad.TabIndex = 8;
+            buttonLoad.Text = "Cargar";
+            buttonLoad.UseVisualStyleBackColor = true;
+            buttonLoad.Click += buttonLoad_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(253, 107);
+            label4.Name = "label4";
+            label4.Size = new Size(96, 15);
+            label4.TabIndex = 7;
+            label4.Text = "Limite de credito";
+            // 
+            // textBoxLimitCredit
+            // 
+            textBoxLimitCredit.Location = new Point(358, 104);
+            textBoxLimitCredit.Name = "textBoxLimitCredit";
+            textBoxLimitCredit.Size = new Size(155, 23);
+            textBoxLimitCredit.TabIndex = 6;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(253, 60);
+            label3.Name = "label3";
+            label3.Size = new Size(105, 15);
+            label3.TabIndex = 5;
+            label3.Text = "Nombre completo";
+            // 
+            // textBoxName
+            // 
+            textBoxName.Location = new Point(358, 57);
+            textBoxName.Name = "textBoxName";
+            textBoxName.Size = new Size(155, 23);
+            textBoxName.TabIndex = 4;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(19, 107);
+            label2.Name = "label2";
+            label2.Size = new Size(41, 15);
+            label2.TabIndex = 3;
+            label2.Text = "Deuda";
+            // 
+            // textBoxDebt
+            // 
+            textBoxDebt.Location = new Point(71, 104);
+            textBoxDebt.Name = "textBoxDebt";
+            textBoxDebt.Size = new Size(136, 23);
+            textBoxDebt.TabIndex = 2;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(19, 60);
+            label1.Name = "label1";
+            label1.Size = new Size(46, 15);
+            label1.TabIndex = 1;
+            label1.Text = "Codigo";
+            // 
+            // textBoxCode
+            // 
+            textBoxCode.Location = new Point(71, 57);
+            textBoxCode.Name = "textBoxCode";
+            textBoxCode.Size = new Size(136, 23);
+            textBoxCode.TabIndex = 0;
+            // 
+            // groupBox1
+            // 
+            groupBox1.Controls.Add(dataGridView1);
+            groupBox1.Controls.Add(button2);
+            groupBox1.Location = new Point(12, 227);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Size = new Size(533, 305);
+            groupBox1.TabIndex = 9;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Consulta de datos";
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Columns.AddRange(new DataGridViewColumn[] { ColumnCode, ColumnName, ColumnDebt, ColumnLimitCredit });
+            dataGridView1.Location = new Point(19, 37);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(494, 210);
+            dataGridView1.TabIndex = 9;
+            // 
+            // ColumnCode
+            // 
+            ColumnCode.HeaderText = "Codigo";
+            ColumnCode.Name = "ColumnCode";
+            // 
+            // ColumnName
+            // 
+            ColumnName.HeaderText = "ColumnName";
+            ColumnName.Name = "ColumnName";
+            // 
+            // ColumnDebt
+            // 
+            ColumnDebt.HeaderText = "Deuda";
+            ColumnDebt.Name = "ColumnDebt";
+            // 
+            // ColumnLimitCredit
+            // 
+            ColumnLimitCredit.HeaderText = "Limite de crédito";
+            ColumnLimitCredit.Name = "ColumnLimitCredit";
+            // 
+            // button2
+            // 
+            button2.Location = new Point(392, 262);
+            button2.Name = "button2";
+            button2.Size = new Size(121, 28);
+            button2.TabIndex = 8;
+            button2.Text = "Listar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(558, 544);
+            Controls.Add(groupBox1);
+            Controls.Add(groupBoxLoadClient);
+            Name = "Form1";
+            Text = "Gestion de clientes";
+            groupBoxLoadClient.ResumeLayout(false);
+            groupBoxLoadClient.PerformLayout();
+            groupBox1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
         }
 
         #endregion
+
+        private GroupBox groupBoxLoadClient;
+        private Label label4;
+        private TextBox textBoxLimitCredit;
+        private Label label3;
+        private TextBox textBoxName;
+        private Label label2;
+        private TextBox textBoxDebt;
+        private Label label1;
+        private TextBox textBoxCode;
+        private Button buttonLoad;
+        private GroupBox groupBox1;
+        private DataGridView dataGridView1;
+        private DataGridViewTextBoxColumn ColumnCode;
+        private DataGridViewTextBoxColumn ColumnName;
+        private DataGridViewTextBoxColumn ColumnDebt;
+        private DataGridViewTextBoxColumn ColumnLimitCredit;
+        private Button button2;
     }
 }
--- 3 - Vectores constituidos por registros/Form1.cs
+++ 3 - Vectores constituidos por registros/Form1.cs
@@ -6,5 +6,49 @@ namespace _3___Vectores_constituidos_por_registros
         {
             InitializeComponent();
         }
+
+        private struct Client
+        {
+            public int code;
+            public string name;
+            public decimal debt;
+            public decimal limitCredit;
+        }
+
+        Client[] clients = new Client[100];
+
+        int totalClients = 0;
+
+        private void buttonLoad_Click(object sender, EventArgs e)
+        {
+            
+
+            clients[totalClients] = CreateClient();
+
+            ClearAllFields();
+
+            totalClients++;
+        }
+
+        private Client CreateClient()
+        {
+            Client client = new Client();
+
+            client.code = Convert.ToInt32(textBoxCode.Text);
+            client.name = textBoxName.Text;
+            client.debt = Convert.ToDecimal(textBoxDebt.Text);
+            client.limitCredit = Convert.ToDecimal(textBoxLimitCredit.Text);
+
+            return client;
+        }
+
+        private void ClearAllFields()
+        {
+            textBoxCode.Text = string.Empty;
+            textBoxDebt.Text = string.Empty;
+            textBoxName.Text = string.Empty;
+            textBoxLimitCredit.Text = string.Empty;
+        }
+
     }
 }
