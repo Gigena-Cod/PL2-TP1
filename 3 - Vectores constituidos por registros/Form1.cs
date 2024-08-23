c7c99f83ac292cba300c2170ed475f0190559b0b
Nahuel Gigena <nahuel.gigena@hotmail.com>
2024-08-23 01:38:35 -0300

    ADD: Init project

---
+++ 3 - Vectores constituidos por registros/Form1.cs
@@ -0,0 +1,10 @@
+namespace _3___Vectores_constituidos_por_registros
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
