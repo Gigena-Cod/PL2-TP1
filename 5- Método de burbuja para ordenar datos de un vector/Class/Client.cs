3185d7a26274d2afe1129f8f38441d3ddb4848f0
Nahuel Gigena <nahuel.gigena@hotmail.com>
2024-08-24 00:40:59 -0300

    ADD:Base

---
+++ 5- Método de burbuja para ordenar datos de un vector/Class/Client.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5__Método_de_burbuja_para_ordenar_datos_de_un_vector
+{
+    internal class Client
+    {
+    }
+}
---
+++ 5- Método de burbuja para ordenar datos de un vector/Form1.cs
@@ -0,0 +1,10 @@
+namespace _5__Método_de_burbuja_para_ordenar_datos_de_un_vector
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+    }
+}
