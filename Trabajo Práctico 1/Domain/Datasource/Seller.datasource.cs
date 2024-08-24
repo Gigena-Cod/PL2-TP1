39755243a5abb49b754ad1b94002c2e9ef87ebce
Nahuel Gigena <nahuel.gigena@hotmail.com>
2024-08-24 15:03:36 -0300

    ADD: Globals
    Create a module in the system and declare public data

---
+++ Trabajo Práctico 1/Domain/Datasource/Seller.datasource.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using static Trabajo_Práctico_1.Domain.Models.Seller;
+
+namespace Trabajo_Práctico_1.Domain.Datasource
+{
+    internal class SellerDatasource
+    {
+        public const int MAXIMUN_SELLERS = 10000;
+
+        public SellerStruct[] sellers = new SellerStruct[MAXIMUN_SELLERS];
+
+        public int totalSellers = 0;
+
+    }
+
+}
---
+++ Trabajo Práctico 1/Domain/Models/Seller.model.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajo_Práctico_1.Domain.Models
+{
+    internal class Seller
+    {
+        public struct SellerStruct
+        {
+            int code;
+            string name;
+            decimal salary;
+        }
+
+    }
+}
