3893c0903145a788ae5da2ff489a7e25b08a947f
Nahuel Gigena <nahuel.gigena@hotmail.com>
2024-08-24 19:01:15 -0300

    ADD: Search seller

---
+++ Trabajo Práctico 1/Infrastructure/Feature/SearchSeller/SearchSeller.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Trabajo_Práctico_1.Infrastructure.Feature.SearchSeller
+{
+    public partial class SearchSeller : Form
+    {
+        public SearchSeller()
+        {
+            InitializeComponent();
+        }
+    }
+}
