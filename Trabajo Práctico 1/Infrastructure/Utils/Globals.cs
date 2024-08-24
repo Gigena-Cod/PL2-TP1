d139054473961ce050f5962ac29fbd397b798f37
Nahuel Gigena <nahuel.gigena@hotmail.com>
2024-08-24 18:38:17 -0300

    ADD: Various changes

---
+++ Trabajo Práctico 1/Infrastructure/Utils/Globals.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Trabajo_Práctico_1.Infrastructure.Utils
+{
+    internal class Globals
+    {
+    }
+}
