9e8085b0b2e6aec1ecae277e5a352634923601f2
Nahuel Gigena <nahuel.gigena@hotmail.com>
2024-08-25 18:58:46 -0300

    ADD: base

--- Módulo 2 - Archivos de texto/Grabar y leer un archivo de texto/Form1.Designer.cs
+++ Módulo 2 - Archivos de texto/Grabar y leer un archivo de texto/Form1.Designer.cs
@@ -28,12 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            label1 = new Label();
+            label2 = new Label();
+            button1 = new Button();
+            textBox1 = new TextBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 9);
+            label1.MaximumSize = new Size(350, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(348, 45);
+            label1.TabIndex = 0;
+            label1.Text = "Este formulario le permite guardar los datos ingresados de manera segura y eficiente. Asegúrese de que toda la información es correcta antes de confirmar el guardado";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 88);
+            label2.Name = "label2";
+            label2.Size = new Size(35, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Texto";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(259, 159);
+            button1.Name = "button1";
+            button1.Size = new Size(101, 33);
+            button1.TabIndex = 2;
+            button1.Text = "Grabar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(12, 116);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(348, 23);
+            textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(372, 206);
+            Controls.Add(textBox1);
+            Controls.Add(button1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Grabar datos";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label label1;
+        private Label label2;
+        private Button button1;
+        private TextBox textBox1;
     }
 }
