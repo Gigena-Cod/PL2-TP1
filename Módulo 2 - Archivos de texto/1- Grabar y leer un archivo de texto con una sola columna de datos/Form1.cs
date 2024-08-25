6a0193a764e99f095e4559141ffb6e897211a89b
Nahuel Gigena <nahuel.gigena@hotmail.com>
2024-08-25 18:23:30 -0300

    MODIFIED: Directories

---
+++ Módulo 2 - Archivos de texto/1- Grabar y leer un archivo de texto con una sola columna de datos/Form1.cs
@@ -0,0 +1,9 @@
+namespace _1__Grabar_y_leer_un_archivo_de_texto_con_una_sola_columna_de_datos;
+
+public partial class Form1 : Form
+{
+    public Form1()
+    {
+        InitializeComponent();
+    }
+}
