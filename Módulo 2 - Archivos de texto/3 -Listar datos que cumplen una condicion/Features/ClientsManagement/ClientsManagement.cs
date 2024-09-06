f8eec20515167950e7e4557ec9b5ac63d1b79a12
Nahuel Gigena <nahuel.gigena@hotmail.com>
2024-09-06 13:58:40 -0300

    ADD: Base

---
+++ Módulo 2 - Archivos de texto/3 -Listar datos que cumplen una condicion/Features/ClientsManagement/ClientsManagement.cs
@@ -0,0 +1,46 @@
+using _2_varias_columnas_de_datos.Feature.CreateClient;
+using _2_varias_columnas_de_datos.Feature.ListClients;
+
+namespace _2_varias_columnas_de_datos
+{
+    public partial class ClientsManagement : Form
+    {
+        public ClientsManagement()
+        {
+            InitializeComponent();
+        }
+
+        private void nueToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CreateClient createClient = new CreateClient();
+
+            createClient.ShowDialog();
+        }
+
+        private void salirToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void verTodosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ListClients listClients = new ListClients();
+
+            listClients.ShowDialog();
+        }
+
+        private void crearClienteToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            CreateClient createClient = new CreateClient();
+
+            createClient.ShowDialog();
+        }
+
+        private void verTodosLosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ListClients listClients = new ListClients();
+
+            listClients.ShowDialog();
+        }
+    }
+}
