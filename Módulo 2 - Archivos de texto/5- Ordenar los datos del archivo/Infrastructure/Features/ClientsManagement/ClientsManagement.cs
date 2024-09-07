b48cae6ebf3b3c8f564e41c807117d8ade3964d6
Nahuel Gigena <nahuel.gigena@hotmail.com>
2024-09-07 19:07:27 -0300

    ADD: Base

---
+++ Módulo 2 - Archivos de texto/5- Ordenar los datos del archivo/Infrastructure/Features/ClientsManagement/ClientsManagement.cs
@@ -0,0 +1,10 @@
+namespace _5__Ordenar_los_datos_del_archivo
+{
+    public partial class ClientsManagement : Form
+    {
+        public ClientsManagement()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Módulo 2 - Archivos de texto/5- Ordenar los datos del archivo/Program.cs
@@ -0,0 +1,17 @@
+namespace _5__Ordenar_los_datos_del_archivo
+{
+    internal static class Program
+    {
+        /// <summary>
+        ///  The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            // To customize application configuration such as set high DPI settings or default font,
+            // see https://aka.ms/applicationconfiguration.
+            ApplicationConfiguration.Initialize();
+            Application.Run(new ClientsManagement());
+        }
+    }
+}
