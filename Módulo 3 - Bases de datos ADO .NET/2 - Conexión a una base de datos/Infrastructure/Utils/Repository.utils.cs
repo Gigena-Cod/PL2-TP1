c709b5c2e95f5129e193c152cd7ccdf61d43f89c
Nahuel Gigena <nahuel.gigena@hotmail.com>
2024-10-06 00:36:55 -0300

    ADD: repository

---
+++ Módulo 3 - Bases de datos ADO .NET/2 - Conexión a una base de datos/Infrastructure/Utils/Repository.utils.cs
@@ -0,0 +1,10 @@
+﻿namespace _2___Conexión_a_una_base_de_datos.Infrastructure.Utils
+{
+    public static class UtilsRepository
+    {
+        public const string ACCESS_URL = "Provider=Microsoft.ACE.OLEDB.12.0;Data Source=DB_USERS.mdb";
+
+        public const string ENTITY_USER = "User";
+
+    }
+}
