23d56004305b1df14d5b86b96b6dee7575d5bfad
Nahuel Gigena <nahuel.gigena@hotmail.com>
2024-10-06 16:19:24 -0300

    ADD: Clase

---
+++ Módulo 3 - Bases de datos ADO .NET/3 - Leer con DataReader/Infrastructure/Features/CustomersList/CustomersList.cs
@@ -0,0 +1,40 @@
+﻿
+using _2___Conexión_a_una_base_de_datos.Domain.Services.Customers;
+using Domain;
+namespace Infrastructure.Features
+{
+    public partial class CustomersList : Form
+    {
+        CustomerServices customerServices = new();
+        public CustomersList()
+        {
+            InitializeComponent();
+        }
+
+        private void CustomersList_Load(object sender, EventArgs e)
+        {
+            dataGridViewCustomers.Rows.Clear();
+
+
+            this.labelTotalCustomers.Text = "Cargando";
+            this.labelAverageDebts.Text = "";
+            this.labelTotalDebts.Text = "";
+
+            ResponseGetAllCustomers? response = customerServices.GetAllUsers();
+
+            if (response.Customers == null || response.Customers.Count == 0) return;
+
+            foreach (Customer customerItem in response.Customers)
+            {
+
+                dataGridViewCustomers.Rows.Add(customerItem.Id, customerItem.Name, customerItem.LastName, customerItem.Email , customerItem.Debt.ToString("C"));
+            }
+
+            this.labelTotalCustomers.Text = $"{response.TotalCustomer} resultados";
+            this.labelAverageDebts.Text = response.AverageDebts.ToString("C");
+            this.labelTotalDebts.Text = response.TotalDebts.ToString("C");
+
+        }
+
+    }
+}
