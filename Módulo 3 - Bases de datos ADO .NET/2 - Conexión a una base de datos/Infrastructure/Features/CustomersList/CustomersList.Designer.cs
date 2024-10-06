8f3a54a084943566aca54f06fcb8d5779eefad8e
Nahuel Gigena <nahuel.gigena@hotmail.com>
2024-10-06 01:04:32 -0300

    MODIFIED: Label

--- Módulo 3 - Bases de datos ADO .NET/2 - Conexión a una base de datos/Infrastructure/Features/CustomersList/CustomersList.Designer.cs
+++ Módulo 3 - Bases de datos ADO .NET/2 - Conexión a una base de datos/Infrastructure/Features/CustomersList/CustomersList.Designer.cs
@@ -30,15 +30,11 @@
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CustomersList));
             dataGridViewCustomers = new DataGridView();
-            label2 = new Label();
-            label1 = new Label();
-            label3 = new Label();
-            label4 = new Label();
-            button1 = new Button();
             ColumnId = new DataGridViewTextBoxColumn();
             ColumnEmail = new DataGridViewTextBoxColumn();
             ColumnName = new DataGridViewTextBoxColumn();
             ColumnLastName = new DataGridViewTextBoxColumn();
+            label2 = new Label();
             ((System.ComponentModel.ISupportInitialize)dataGridViewCustomers).BeginInit();
             SuspendLayout();
             // 
@@ -46,59 +42,10 @@
             // 
             dataGridViewCustomers.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
             dataGridViewCustomers.Columns.AddRange(new DataGridViewColumn[] { ColumnId, ColumnEmail, ColumnName, ColumnLastName });
-            dataGridViewCustomers.Location = new Point(12, 157);
+            dataGridViewCustomers.Location = new Point(12, 101);
             dataGridViewCustomers.Name = "dataGridViewCustomers";
-            dataGridViewCustomers.Size = new Size(733, 264);
+            dataGridViewCustomers.Size = new Size(540, 337);
             dataGridViewCustomers.TabIndex = 0;
-            // 
-            // label2
-            // 
-            label2.AutoSize = true;
-            label2.Location = new Point(12, 19);
-            label2.MaximumSize = new Size(550, 0);
-            label2.Name = "label2";
-            label2.Size = new Size(549, 60);
-            label2.TabIndex = 2;
-            label2.Text = resources.GetString("label2.Text");
-            // 
-            // label1
-            // 
-            label1.AutoSize = true;
-            label1.Location = new Point(12, 103);
-            label1.MaximumSize = new Size(550, 0);
-            label1.Name = "label1";
-            label1.Size = new Size(56, 15);
-            label1.TabIndex = 3;
-            label1.Text = "9 clientes";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Location = new Point(102, 103);
-            label3.MaximumSize = new Size(550, 0);
-            label3.Name = "label3";
-            label3.Size = new Size(84, 15);
-            label3.TabIndex = 4;
-            label3.Text = "Total de deuda";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Location = new Point(275, 103);
-            label4.MaximumSize = new Size(550, 0);
-            label4.Name = "label4";
-            label4.Size = new Size(111, 15);
-            label4.TabIndex = 5;
-            label4.Text = "Promedio de deuda";
-            // 
-            // button1
-            // 
-            button1.Location = new Point(637, 19);
-            button1.Name = "button1";
-            button1.Size = new Size(108, 29);
-            button1.TabIndex = 6;
-            button1.Text = "Generar reporte";
-            button1.UseVisualStyleBackColor = true;
             // 
             // ColumnId
             // 
@@ -124,15 +71,21 @@
             ColumnLastName.HeaderText = "Apellido";
             ColumnLastName.Name = "ColumnLastName";
             // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 19);
+            label2.MaximumSize = new Size(550, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(540, 45);
+            label2.TabIndex = 2;
+            label2.Text = resources.GetString("label2.Text");
+            // 
             // CustomersList
             // 
             AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(760, 450);
-            Controls.Add(button1);
-            Controls.Add(label4);
-            Controls.Add(label3);
-            Controls.Add(label1);
+            ClientSize = new Size(567, 450);
             Controls.Add(label2);
             Controls.Add(dataGridViewCustomers);
             Name = "CustomersList";
@@ -147,10 +100,6 @@
 
         private DataGridView dataGridViewCustomers;
         private Label label2;
-        private Label label1;
-        private Label label3;
-        private Label label4;
-        private Button button1;
         private DataGridViewTextBoxColumn ColumnId;
         private DataGridViewTextBoxColumn ColumnEmail;
         private DataGridViewTextBoxColumn ColumnName;
