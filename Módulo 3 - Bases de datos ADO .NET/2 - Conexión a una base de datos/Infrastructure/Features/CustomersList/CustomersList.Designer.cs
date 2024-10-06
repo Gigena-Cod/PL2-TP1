727db538cf74c93eadb0879d302f235cfa607f1d
Nahuel Gigena <nahuel.gigena@hotmail.com>
2024-10-05 22:26:55 -0300

    ADD:Table

---
+++ Módulo 3 - Bases de datos ADO .NET/2 - Conexión a una base de datos/Infrastructure/Features/CustomersList/CustomersList.Designer.cs
@@ -0,0 +1,126 @@
+﻿namespace Infrastructure.Features
+{
+    partial class CustomersList
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CustomersList));
+            dataGridView1 = new DataGridView();
+            label2 = new Label();
+            label1 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            button1 = new Button();
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).BeginInit();
+            SuspendLayout();
+            // 
+            // dataGridView1
+            // 
+            dataGridView1.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridView1.Location = new Point(12, 157);
+            dataGridView1.Name = "dataGridView1";
+            dataGridView1.Size = new Size(733, 264);
+            dataGridView1.TabIndex = 0;
+            dataGridView1.CellContentClick += this.dataGridView1_CellContentClick;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(12, 19);
+            label2.MaximumSize = new Size(550, 0);
+            label2.Name = "label2";
+            label2.Size = new Size(549, 60);
+            label2.TabIndex = 2;
+            label2.Text = resources.GetString("label2.Text");
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 103);
+            label1.MaximumSize = new Size(550, 0);
+            label1.Name = "label1";
+            label1.Size = new Size(56, 15);
+            label1.TabIndex = 3;
+            label1.Text = "9 clientes";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(102, 103);
+            label3.MaximumSize = new Size(550, 0);
+            label3.Name = "label3";
+            label3.Size = new Size(84, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Total de deuda";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(275, 103);
+            label4.MaximumSize = new Size(550, 0);
+            label4.Name = "label4";
+            label4.Size = new Size(111, 15);
+            label4.TabIndex = 5;
+            label4.Text = "Promedio de deuda";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(637, 19);
+            button1.Name = "button1";
+            button1.Size = new Size(108, 29);
+            button1.TabIndex = 6;
+            button1.Text = "Generar reporte";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // CustomersList
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(760, 450);
+            Controls.Add(button1);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label1);
+            Controls.Add(label2);
+            Controls.Add(dataGridView1);
+            Name = "CustomersList";
+            Text = "Listado de clientes";
+            ((System.ComponentModel.ISupportInitialize)dataGridView1).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private DataGridView dataGridView1;
+        private Label label2;
+        private Label label1;
+        private Label label3;
+        private Label label4;
+        private Button button1;
+    }
+}
---
+++ Módulo 3 - Bases de datos ADO .NET/2 - Conexión a una base de datos/Infrastructure/Features/CustomersList/CustomersList.cs
@@ -0,0 +1,11 @@
+﻿
+namespace Infrastructure.Features
+{
+    public partial class CustomersList : Form
+    {
+        public CustomersList()
+        {
+            InitializeComponent();
+        }
+    }
+}
