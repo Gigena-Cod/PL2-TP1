d264299594e285c58f748730d3015e2b01ed27c2
Nahuel Gigena <nahuel.gigena@hotmail.com>
2024-10-19 11:52:42 -0300

    ADD: Base project

---
+++ Módulo 3 - Bases de datos ADO .NET/6 - Agregar un nuevo registro/Infrastructure/Features/CustomerManager/CustomerManager.Designer.cs
@@ -0,0 +1,144 @@
+﻿namespace _6__Agregar_un_nuevo_registro
+{
+    partial class CustomerManager
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new MenuStrip();
+            sistemaToolStripMenuItem = new ToolStripMenuItem();
+            acervToolStripMenuItem = new ToolStripMenuItem();
+            salirToolStripMenuItem = new ToolStripMenuItem();
+            clientesToolStripMenuItem = new ToolStripMenuItem();
+            crearToolStripMenuItem = new ToolStripMenuItem();
+            dToolStripMenuItem = new ToolStripMenuItem();
+            visualizarLosClienteToolStripMenuItem = new ToolStripMenuItem();
+            buscarClienteToolStripMenuItem = new ToolStripMenuItem();
+            toolStripSeparator1 = new ToolStripSeparator();
+            toolStripSeparator2 = new ToolStripSeparator();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.Items.AddRange(new ToolStripItem[] { sistemaToolStripMenuItem, clientesToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(800, 24);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // sistemaToolStripMenuItem
+            // 
+            sistemaToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { acervToolStripMenuItem, salirToolStripMenuItem });
+            sistemaToolStripMenuItem.Name = "sistemaToolStripMenuItem";
+            sistemaToolStripMenuItem.Size = new Size(60, 20);
+            sistemaToolStripMenuItem.Text = "Sistema";
+            // 
+            // acervToolStripMenuItem
+            // 
+            acervToolStripMenuItem.Name = "acervToolStripMenuItem";
+            acervToolStripMenuItem.Size = new Size(126, 22);
+            acervToolStripMenuItem.Text = "Acerca de";
+            // 
+            // salirToolStripMenuItem
+            // 
+            salirToolStripMenuItem.Name = "salirToolStripMenuItem";
+            salirToolStripMenuItem.Size = new Size(180, 22);
+            salirToolStripMenuItem.Text = "Salir";
+            // 
+            // clientesToolStripMenuItem
+            // 
+            clientesToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { crearToolStripMenuItem, toolStripSeparator1, visualizarLosClienteToolStripMenuItem, buscarClienteToolStripMenuItem, toolStripSeparator2, dToolStripMenuItem });
+            clientesToolStripMenuItem.Name = "clientesToolStripMenuItem";
+            clientesToolStripMenuItem.Size = new Size(61, 20);
+            clientesToolStripMenuItem.Text = "Clientes";
+            // 
+            // crearToolStripMenuItem
+            // 
+            crearToolStripMenuItem.Name = "crearToolStripMenuItem";
+            crearToolStripMenuItem.Size = new Size(180, 22);
+            crearToolStripMenuItem.Text = "Agregar cliente";
+            // 
+            // dToolStripMenuItem
+            // 
+            dToolStripMenuItem.Name = "dToolStripMenuItem";
+            dToolStripMenuItem.Size = new Size(180, 22);
+            dToolStripMenuItem.Text = "Eliminar cliente";
+            // 
+            // visualizarLosClienteToolStripMenuItem
+            // 
+            visualizarLosClienteToolStripMenuItem.Name = "visualizarLosClienteToolStripMenuItem";
+            visualizarLosClienteToolStripMenuItem.Size = new Size(184, 22);
+            visualizarLosClienteToolStripMenuItem.Text = "Visualizar los clientes";
+            // 
+            // buscarClienteToolStripMenuItem
+            // 
+            buscarClienteToolStripMenuItem.Name = "buscarClienteToolStripMenuItem";
+            buscarClienteToolStripMenuItem.Size = new Size(184, 22);
+            buscarClienteToolStripMenuItem.Text = "Buscar cliente";
+            // 
+            // toolStripSeparator1
+            // 
+            toolStripSeparator1.Name = "toolStripSeparator1";
+            toolStripSeparator1.Size = new Size(181, 6);
+            // 
+            // toolStripSeparator2
+            // 
+            toolStripSeparator2.Name = "toolStripSeparator2";
+            toolStripSeparator2.Size = new Size(181, 6);
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Administrador de clientes";
+            WindowState = FormWindowState.Maximized;
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private MenuStrip menuStrip1;
+        private ToolStripMenuItem sistemaToolStripMenuItem;
+        private ToolStripMenuItem acervToolStripMenuItem;
+        private ToolStripMenuItem salirToolStripMenuItem;
+        private ToolStripMenuItem clientesToolStripMenuItem;
+        private ToolStripMenuItem crearToolStripMenuItem;
+        private ToolStripMenuItem dToolStripMenuItem;
+        private ToolStripMenuItem visualizarLosClienteToolStripMenuItem;
+        private ToolStripMenuItem buscarClienteToolStripMenuItem;
+        private ToolStripSeparator toolStripSeparator1;
+        private ToolStripSeparator toolStripSeparator2;
+    }
+}
---
+++ Módulo 3 - Bases de datos ADO .NET/6 - Agregar un nuevo registro/Infrastructure/Features/CustomerManager/CustomerManager.cs
@@ -0,0 +1,10 @@
+namespace _6__Agregar_un_nuevo_registro
+{
+    public partial class CustomerManager : Form
+    {
+        public CustomerManager()
+        {
+            InitializeComponent();
+        }
+    }
+}
