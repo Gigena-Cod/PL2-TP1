5fca0fc6d32231aa363355d03128583f9f108ac5
Nahuel Gigena <nahuel.gigena@hotmail.com>
2024-10-19 12:09:33 -0300

    ADD: Create customer form

---
+++ Módulo 3 - Bases de datos ADO .NET/6 - Agregar un nuevo registro/Infrastructure/Features/CreateCustomer/CreateCustomer.Designer.cs
@@ -0,0 +1,278 @@
+﻿namespace _6__Agregar_un_nuevo_registro.Infrastructure.Features.CreateCustomer
+{
+    partial class CreateCustomer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(CreateCustomer));
+            label1 = new Label();
+            label2 = new Label();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            label3 = new Label();
+            label4 = new Label();
+            textBox3 = new TextBox();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            textBox7 = new TextBox();
+            label10 = new Label();
+            label11 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            comboBox1 = new ComboBox();
+            textBox4 = new TextBox();
+            comboBox2 = new ComboBox();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.Location = new Point(12, 16);
+            label1.Name = "label1";
+            label1.Size = new Size(483, 65);
+            label1.TabIndex = 0;
+            label1.Text = resources.GetString("label1.Text");
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(15, 102);
+            label2.Name = "label2";
+            label2.Size = new Size(51, 15);
+            label2.TabIndex = 1;
+            label2.Text = "Nombre";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(15, 130);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(234, 23);
+            textBox1.TabIndex = 2;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(261, 130);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(234, 23);
+            textBox2.TabIndex = 3;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(261, 102);
+            label3.Name = "label3";
+            label3.Size = new Size(51, 15);
+            label3.TabIndex = 4;
+            label3.Text = "Apellido";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(12, 301);
+            label4.Name = "label4";
+            label4.Size = new Size(57, 15);
+            label4.TabIndex = 5;
+            label4.Text = "Direccion";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(12, 329);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(237, 23);
+            textBox3.TabIndex = 6;
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(261, 301);
+            label5.Name = "label5";
+            label5.Size = new Size(45, 15);
+            label5.TabIndex = 7;
+            label5.Text = "Ciudad";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(13, 364);
+            label6.Name = "label6";
+            label6.Size = new Size(56, 15);
+            label6.TabIndex = 9;
+            label6.Text = "Provincia";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(261, 364);
+            label7.Name = "label7";
+            label7.Size = new Size(28, 15);
+            label7.TabIndex = 11;
+            label7.Text = "Pais";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label8.Location = new Point(12, 271);
+            label8.Name = "label8";
+            label8.Size = new Size(60, 15);
+            label8.TabIndex = 13;
+            label8.Text = "Ubicacion";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label9.Location = new Point(15, 74);
+            label9.Name = "label9";
+            label9.Size = new Size(76, 15);
+            label9.TabIndex = 14;
+            label9.Text = "Informacion ";
+            // 
+            // textBox7
+            // 
+            textBox7.Location = new Point(15, 226);
+            textBox7.Name = "textBox7";
+            textBox7.Size = new Size(234, 23);
+            textBox7.TabIndex = 15;
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(12, 197);
+            label10.Name = "label10";
+            label10.Size = new Size(40, 15);
+            label10.TabIndex = 16;
+            label10.Text = "Limite";
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Font = new Font("Segoe UI Semibold", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            label11.Location = new Point(12, 170);
+            label11.Name = "label11";
+            label11.Size = new Size(56, 15);
+            label11.TabIndex = 17;
+            label11.Text = "Crediticia";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(261, 447);
+            button1.Name = "button1";
+            button1.Size = new Size(89, 30);
+            button1.TabIndex = 18;
+            button1.Text = "Agregar";
+            button1.UseVisualStyleBackColor = true;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(160, 447);
+            button2.Name = "button2";
+            button2.Size = new Size(89, 30);
+            button2.TabIndex = 19;
+            button2.Text = "Cancelar";
+            button2.UseVisualStyleBackColor = true;
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Location = new Point(261, 391);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(234, 23);
+            comboBox1.TabIndex = 20;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(261, 329);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(234, 23);
+            textBox4.TabIndex = 8;
+            // 
+            // comboBox2
+            // 
+            comboBox2.FormattingEnabled = true;
+            comboBox2.Location = new Point(15, 391);
+            comboBox2.Name = "comboBox2";
+            comboBox2.Size = new Size(234, 23);
+            comboBox2.TabIndex = 21;
+            // 
+            // CreateCustomer
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(507, 489);
+            Controls.Add(comboBox2);
+            Controls.Add(comboBox1);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label11);
+            Controls.Add(label10);
+            Controls.Add(textBox7);
+            Controls.Add(label9);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(textBox4);
+            Controls.Add(label5);
+            Controls.Add(textBox3);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "CreateCustomer";
+            Text = "Crear cliente";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private Label label3;
+        private Label label4;
+        private TextBox textBox3;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private TextBox textBox7;
+        private Label label10;
+        private Label label11;
+        private Button button1;
+        private Button button2;
+        private ComboBox comboBox1;
+        private TextBox textBox4;
+        private ComboBox comboBox2;
+    }
+}
---
+++ Módulo 3 - Bases de datos ADO .NET/6 - Agregar un nuevo registro/Infrastructure/Features/CreateCustomer/CreateCustomer.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _6__Agregar_un_nuevo_registro.Infrastructure.Features.CreateCustomer
+{
+    public partial class CreateCustomer : Form
+    {
+        public CreateCustomer()
+        {
+            InitializeComponent();
+        }
+    }
+}
